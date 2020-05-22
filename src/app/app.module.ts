2aa45d90aede5d2d203f1e2a1454ac88efce55bb
Shamsul Hasan Siam <shamsulhasansiam@gmail.com>
2020-05-22 19:41:00 +0600

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 import 'hammerjs';
@@ -18,6 +19,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import {DishService} from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -33,6 +40,7 @@ import { FooterComponent } from './footer/footer.component';
     MatToolbarModule,
     FlexLayoutModule,
     MatGridListModule,
+    AppRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatListModule
