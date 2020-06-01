ffa808a1495cacbf7f5624fe76ec7a85c43da6ee
Shamsul Hasan Siam <shamsulhasansiam@gmail.com>
2020-06-01 18:13:02 +0600

    RxJS Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -47,4 +47,8 @@ export class DishService {
     //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 
+  getDishIds(): Observable<string[] | any> {
+    return of(DISHES.map(dish => dish.id ));
+  }
+
 }
